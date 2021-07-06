3739562e05875666498f7b51a4e4f476a604cbcb
Little Clover <littleclover909@gmail.com>
2021-07-06 18:19:47 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# souvenir.nvim
+A Neovim plugin that manages Neovim sessions
+
+## Requirement
+Neovim 0.5+
+
+[plenary.nvim](https://github.com/nvim-lua/plenary.nvim)
+
+## Installation
+vim-plug
+
+```viml
+Plug 'ingram1107/souvenir.nvim'
+Plug 'nvim-lua/plenary.nvim'
+```
+
+packer
+
+```lua
+use {
+  'ingram1107/souvenir.nvim',
+  requires = 'nvim-lua/plenary.nvim',
+}
+```
+
+## Usage
+
+You could set-up your session path as follows:
+
+```lua
+require('souvenir').setup {
+  session_path = '~/.config/nvim/sessions/' -- a backslash is needed
+}
+```
+
+Or else the default session path would be set to
+`$XDG_DATA_HOME/nvim/souvenirs/` or `$HOME/.local/share/nvim/souvenirs/`
+according to your system environment variables. Currently, this plugin only
+support Linux path. (Do not work in Windows, may not work in macOS and
+BSD-variants)
+
+souvenir.nvim currently provides 4 functionalities: save Vim session, restore
+Vim session, delete Vim session and list Vim sessions. Commands to call these 4
+functionalities are shown as below:
+
+```vimL
+:lua require('souvenir').save_session{'souvenir'} -- don't override existing
+session file
+:lua require('souvenir').save_session{'souvenir', true} -- override exisitng
+session file
+:lua require('souvenir').restore_session('souvenir')
+:lua require('souvenir').delete_session('souvenir')
+:lua require('souvenir').list_session()
+```
+
+## Inspiration
+
+[xolox/vim-session](https://github.com/xolox/vim-session)
+
+## Todo
+- [ ] documentation
+- [x] save session to a pre-configured location
+- [x] restore session
+- [x] delete session
+  - [ ] delete multiple sessions
+- [x] list sessions
+- [ ] interactive buffer?
+- [ ] cross-platform
+  - [ ] Windows
+  - [x] Linux
+  - [ ] macOS
+  - [ ] BSD-variants
---
+++ lua/souvenir.lua
@@ -0,0 +1,169 @@
+--[[
+  souvenir.nvim   Nvim plugin that manages Neovim sessions
+  Copyright (C) 2021  Little Clover 
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+  GNU General Public License for more details. 
+  You should have received a copy of the GNU General Public License 
+  along with this program.  If not, see <https://www.gnu.org/licenses/>
+--]]
+local scan = require('plenary').scandir
+
+if vim.version().minor < 5 then
+  vim.api.nvim_err_writeln('fatal: souvenir: require Neovim version 0.5+')
+  return
+end
+
+local SHADA_PATH = (function()
+  if vim.env.XDG_DATA_HOME ~= nil then
+    return vim.env.XDG_DATA_HOME..'/nvim/shada/'
+  else
+    return vim.env.HOME..'.local/share/nvim/shada/'
+  --[[
+    TODO:
+        add window path support
+          - littleclover 2021-07-03 11:59:47 PM +0800
+  --]]
+  end
+end)()
+local session_path = nil
+
+local function set_session_path(path)
+  session_path = vim.fn.expand(path)
+end
+
+local function is_dir_exist(dir)
+  if vim.fn.isdirectory(dir) == 1 then
+    return true
+  end
+  return false
+end
+
+local function is_file_exist(file)
+  if vim.fn.empty(vim.fn.glob(file)) ~= 1 and vim.fn.filereadable(vim.fn.glob(file)) == 1 then
+    return true
+  end
+  return false
+end
+
+local function create_dir_recur(path)
+  if vim.fn.mkdir(path, 'p') == false then
+    vim.api.nvim_err_writeln('fatal: cannot create directory '..path)
+  else
+    vim.api.nvim_echo({{'souvenir: directory '..path..' successfully created', 'Normal'}}, true, {})
+  end
+end
+
+local function session_path_init()
+  if session_path == '' or session_path == nil then
+    if vim.env.XDG_DATA_HOME ~= nil then
+      session_path = vim.env.XDG_DATA_HOME..'/nvim/souvenirs/'
+      if is_dir_exist(session_path) == false then
+        create_dir_recur(session_path)
+      end
+    else
+      session_path = vim.env.HOME..'/.local/share/nvim/souvenirs/'
+      if is_dir_exist(session_path) == false then
+        create_dir_recur(session_path)
+      end
+    end
+    --[[
+    TODO:
+    add window path support
+    - littleclover 2021-07-03 11:59:47 PM +0800
+    --]]
+  else
+    if is_dir_exist(session_path) == false then
+      create_dir_recur(session_path)
+    end
+  end
+
+  return session_path
+end
+
+local function save_session(args)
+  setmetatable(args, { __index = { override = false } })
+  local session_file = args[1]..'.vim' or args.session..'.vim'
+  local session_shada = args[1]..'.shada' or args.session..'.shada'
+  local override = args[2] or args.override
+
+  session_path = session_path_init()
+
+  if override == false and is_file_exist(session_path..session_file) == false then
+    vim.api.nvim_exec('mksession '..session_path..session_file, false)
+    vim.api.nvim_exec('wshada! '..SHADA_PATH..session_shada, false)
+  elseif override == true then
+    vim.api.nvim_exec('mksession! '..session_path..session_file, false)
+    vim.api.nvim_exec('wshada! '..SHADA_PATH..session_shada, false)
+  else
+    vim.api.nvim_err_writeln('fatal: '..session_file..' exists! pass second arg as `true` to `save_session` to override the file')
+  end
+end
+
+local function restore_session(session)
+  local session_file = session..'.vim'
+  local session_shada = session..'.shada'
+
+  session_path = session_path_init()
+
+  if is_file_exist(session_path..session_file) == true then
+    vim.api.nvim_exec('source '..session_path..session_file, false)
+    vim.api.nvim_exec('rshada! '..SHADA_PATH..session_shada, false)
+  else
+    vim.api.nvim_err_writeln('fatal: '..session_file..' does not exist')
+  end
+end
+
+local function list_session()
+  session_path = session_path_init()
+
+  if is_dir_exist(session_path) == true then
+    local file_list_tbl = scan.scan_dir(session_path, { depth = 1 })
+    print('Session List:')
+    print(' ')
+    for i, file in ipairs(file_list_tbl) do
+      local base_file = vim.fn.substitute(file, '.*/\\ze', '', '')
+      print('    '..i..'. '..vim.fn.substitute(base_file, '\\.vim', '', ''))
+    end
+  else
+    vim.api.nvim_echo({{'souvenir: no session has been stored', 'Normal'}}, true, {})
+  end
+end
+
+local function delete_session(session)
+  local session_file = session..'.vim'
+  local session_shada = session..'.shada'
+
+  session_path = session_path_init()
+
+  if is_file_exist(session_path..session_file) == true then
+    vim.fn.delete(session_path..session_file)
+    vim.fn.delete(SHADA_PATH..session_shada)
+    vim.api.nvim_echo({{'souvenir: session `'..session..'` deleted', 'Normal'}}, true, {})
+  else
+    vim.api.nvim_err_writeln('fatal: '..session_file..' does not exist')
+  end
+end
+
+local function setup(cfg_tbl)
+  local sp = cfg_tbl['session_path']
+
+  if sp ~= nil and sp ~= '' then
+    set_session_path(sp)
+  end
+  -- override = cfg_tbl['override']
+  -- shada = cfg_tbl['shada']
+end
+
+return {
+  save_session    = save_session,
+  restore_session = restore_session,
+  delete_session  = delete_session,
+  list_session    = list_session,
+  setup           = setup,
+}
